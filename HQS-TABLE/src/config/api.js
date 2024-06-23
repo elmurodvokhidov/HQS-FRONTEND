17853a77c2d7f1b23d995e83c52ba6146579d357
elmurodvokhidov <elmurodvkh@gmail.com>
2024-06-23 12:45:19 +0500

    baseUrl bug to'g'irlandi

--- HQS-TABLE/src/config/api.js
+++ HQS-TABLE/src/config/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-export const baseURL = "https://hqs-backend.onrender.com/api";
-const api = axios.create({ baseURL });
+export const baseURL = "https://hqs-backend.onrender.com";
+const api = axios.create({ baseURL: baseURL + "/api" });
 export default api;
